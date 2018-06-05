e6f7700b94213bbe4ab8d40942487548403b3d92
xuyingqi <xyqby1020@163.com>
2018-06-05 22:12:48 +0800

    search finish

--- src/components/city/city.vue
+++ src/components/city/city.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <cityHeader></cityHeader>
-    <citySearch></citySearch>
+    <citySearch :cities="cities"></citySearch>
     <cityList
       :cities="cities"
       :hotCities="hotCities"
