0065fddb06c9da06c147ae4ae5a3921142969ed5
xuyingqi <xyqby1020@163.com>
2018-06-07 17:25:16 +0800

    12

---
+++ src/components/detail/List/list.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div
+      class="item"
+      v-for="(item, index) in list"
+      :key="index">
+      <div class="item-title">
+        <span class="item-title-icon"></span>
+        {{item.title}}
+      </div>
+      <div v-if="item.children" class="item-children">
+        <list :list="item.children"></list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: 'list', // 用于递归组件
+    props: {
+      list: {
+        type: Array
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .item-title-icon
+    position: relative
+    left: .06rem
+    top: .06rem
+    display: inline-block
+    width: .36rem
+    height: .36rem
+    background: url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat
+    margin-right: .1rem
+    background-size: .4rem 3rem
+  .item-title
+    line-height: .8rem
+    font-size: .32rem
+    padding: 0 .2rem
+  .item-children
+    padding: 0 .2rem
+</style>
--- src/components/detail/detail.vue
+++ src/components/detail/detail.vue
@@ -1,27 +1,50 @@
 <template>
-    <div>
-      <v-header></v-header>
-      <banner></banner>
-      <div class="content"></div>
-    </div>
+  <div>
+    <v-header></v-header>
+    <banner></banner>
+    <list :list="list"></list>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
   // import axios from 'axios'
   import banner from 'components/detail/banner/banner'
   import header from 'components/detail/header/header'
+  import list from 'components/detail/list/list'
+
   export default {
+    data() {
+      return {
+        list: [
+          {
+            title: '成人票',
+            children: [{
+              title: '成人三馆联票',
+              children: [
+                {
+                  title: '成人三馆联票-某一连锁店销售'
+                }
+              ]
+            }]
+          }, {
+            title: '学生票'
+          }, {
+            title: '儿童票'
+          }, {
+            title: '特惠票'
+          }
+        ]
+      }
+    },
     methods: {
-      // axios.get().then
     },
     components: {
       banner,
-      'v-header': header
+      'v-header': header,
+      list
     }
   }
 </script>
 
 <style lang="stylus" scoped>
-  .content
-    height: 1000px
 </style>
